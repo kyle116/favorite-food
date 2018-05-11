d5a086660ba8a080d336149e32e13881b94207c1
EC2 Default User <kyle116@yahoo.com>
2018-05-11 22:19:40 +0000

    Database connection

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const 
   express = require("express"),
   bodyParser = require("body-parser"),
+  path = require("path"),
   cors = require("cors"),
   passport = require("passport"),
-  mongoose = require("mongoose");
+  mongoose = require("mongoose"),
+  config = require("./config/database");
+  
+  
+// Connect to database
+mongoose.connect(config.database);
+// Connection Success
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to database ${config.database}`)
+})
+// Connection Error
+mongoose.connection.on("error", (err) => {
+  console.log(`Database error: ${err}`)
+})
   
 const app = express();
 // Helps to redirect to routes when endpoint is hit
@@ -15,7 +29,8 @@ const PORT = 8080;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use("/users", users)
+app.use("/users", users);
+app.use(express.static(path.join(__dirname, "public")));
 
 // Index Route
 app.get("/", (req, res) => {
