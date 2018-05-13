12cbe1e653bfb962bede3094f6b210330c771e5d
Kyle Liu <kyle116@yahoo.com>
2018-05-13 01:11:59 -0700

    guards and route protection

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { map } from 'rxjs/operators';
+import { tokenNotExpired } from 'angular2-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -25,11 +26,29 @@ export class AuthService {
       .pipe(map(res => res.json()));
   }
 
+  getProfile() {
+    let headers = new Headers();
+    this.loadToken();
+    headers.append("Authorization", this.authToken);
+    headers.append("Content-Type", "application/json");
+    return this.http.get("http://localhost:3000/users/profile", {headers: headers})
+      .pipe(map(res => res.json()));
+  }
+
   storeUserData(token, user) {
     localStorage.setItem("id_token", token);
     localStorage.setItem("user", JSON.stringify(user));
     this.authToken = token;
     this.user = user;
+  }
+
+  loadToken() {
+    const token = localStorage.getItem("id_token");
+    this.authToken = token;
+  }
+
+  loggedIn() {
+    return tokenNotExpired("id_token");
   }
 
   logout() {
